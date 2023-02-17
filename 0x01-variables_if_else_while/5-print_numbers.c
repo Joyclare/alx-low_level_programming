f46cd8770ef5d98915ad45621756b611d360847c
Joyclare <cheronojoyclaire@gmail.com>
2023-02-17 09:18:28 -0800

    numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Description: Prints all single digits of base 10 from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+		printf("% d", b);
+	}
+	printf("\n");
+	return (0);
+}
