ace5470ed40cf55997ba375596f9be639e4b0c17
Joyclare <cheronojoyclaire@gmail.com>
2023-04-06 07:21:35 -0700

    Prototypes

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+unsigned int binary_to_uint(cond=st char *b);
+void print_binat=ry(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+unsignd int flip_bits(unsigned long int n, unsigned long int m);
+int _atoi(const char *s);
+int _get_endless(void);
+
+#endif
