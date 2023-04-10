940f18943e669e0d6669b76502189d12e0e1e43c
Joyclare <cheronojoyclaire@gmail.com>
2023-04-10 14:19:19 -0700

    Creates a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,18 +14,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	ssize_t written;
+	ssize_t read;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	read = read(fd, buf, letters);
+	written = write(STDOUT_FILENO, buf, read);
 
-	free(buf);
+	free(buffer);
 	close(fd);
-	return (w);
+	return (written);
 }
