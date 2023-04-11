bb45ecf51f57316d215a64942d4dec9268d3efd3
Joyclare <cheronojoyclaire@gmail.com>
2023-04-11 10:02:01 -0700

    Sets the value of a bit to 0 at a given index

---
+++ 0x14-bit_manipulation/4-main.c
@@ -0,0 +1,26 @@
+/* this is the 4-main.c file for complilation purposes*/
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int n;
+
+	n = 1024;
+	clear_bit(&n, 10);
+	printf("%lu\n", n);
+	n = 0;
+	clear_bit(&n, 10);
+	printf("%lu\n", n);
+	n = 98;
+	clear_bit(&n, 1);
+	printf("%lu\n", n);
+
+	return (0);
+}
