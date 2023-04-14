925c81178002c933a21677eecdf6074dce00a688
Joyclare <cheronojoyclaire@gmail.com>
2023-04-14 05:06:22 -0700

    Reads the text file and prints it to the STDOUT

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,10 +17,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t j;
 	ssize_t k;
 
-	fd = open(filename, O_RDONLY);
+	if (!filename)
+		return (0);
+
+	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+
 	k = read(file_descriptor, buffer, letters);
 	j = write(STDOUT_FILENO, buffer, k);
 
