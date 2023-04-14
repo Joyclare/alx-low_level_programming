88c6427a396e0fa2adefa2b9fe9a9f2ed8eb067c
Joyclare <cheronojoyclaire@gmail.com>
2023-04-14 06:10:24 -0700

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
-#include <stddef.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * create_file - creates a file
@@ -11,25 +12,38 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int j, m, length = 0;
+	int j;
+	int k;
+	int l;
 
 	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length];)
-			length++;
-
+		return -1;
 	}
 
-	j = open(filename, 0_CREAT | O_RDWR | O_TRUNC, 0600);
-	m = write(j, text_content, length);
+	j = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (j == -1 || m == -1)
-		return (-1);
+	if (j == -1)
+	{
+		return -1;
+	}
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	for (k = 0; text_content[k]; k++)
+		;
+
+	l = write(j, text_content, k);
+
+	if (l == -1)
+	{
+		return -1;
+	}
 
 	close(j);
 
-	return (1);
+	return 1;
 }
