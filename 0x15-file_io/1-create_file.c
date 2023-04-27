b2b91a1dc663a08ded6cbb5caac88367bad9a60d
Joyclare <cheronojoyclaire@gmail.com>
2023-04-27 00:15:13 -0700

    Creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,34 +16,23 @@ int create_file(const char *filename, char *text_content)
 	int k;
 	int l;
 
-	if (filename == NULL)
+	if (filename == 0)
+		return (-1);
+
+	if (text_content != 0)
 	{
-		return -1;
+		for (l = 0; text_content[l];)
+			l++;
 	}
 
-	j = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	j = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	k = write(j, text_content, l);
 
-	if (j == -1)
-	{
-		return -1;
-	}
 
-	if (text_content == NULL)
-	{
-		text_content = "";
-	}
-
-	for (k = 0; text_content[k]; k++)
-		;
-
-	l = write(j, text_content, k);
-
-	if (l == -1)
-	{
-		return -1;
-	}
+	if (j == -1 || k == -1)
+		return (-1);
 
 	close(j);
 
-	return 1;
+	return (1);
 }
